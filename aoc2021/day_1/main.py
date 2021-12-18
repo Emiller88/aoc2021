58a6fc552af3f88a2a36da6dd45e9212eb195eb1
Edmund Miller <edmund.a.miller@protonmail.com>
2021-12-17 20:08:46 -0600

    feat(day_1): Finish part 1
    Date:      Fri Dec 17 20:30:46 2021 -0600

---
+++ aoc2021/day_1/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def main():
+    example = pd.DataFrame(
+        [
+            199,
+            200,
+            208,
+            210,
+            200,
+            207,
+            240,
+            269,
+            260,
+            263,
+        ]
+    )
+    print(example)
---
+++ tests/test_day_1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import aoc2021.day_1
+
+def test_main():
+    aoc2021.day_1.main()
