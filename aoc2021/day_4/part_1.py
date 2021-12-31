0f30b573c25deae959165b5d9da49ff919d32347
Edmund Miller <edmund.a.miller@protonmail.com>
2021-12-30 19:17:13 -0600

    fix(day_4): Update the way boards are read in

--- aoc2021/day_4/part_1.py
+++ aoc2021/day_4/part_1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def read_data(input_file_path: str):
     draw_numbers = pd.read_csv(
         input_file_path,
@@ -8,9 +9,12 @@ def read_data(input_file_path: str):
         index_col=False,
     )
 
-    with pd.read_csv(input_file_path, skiprows=2, chunksize=5, header=None) as reader:
+    boards = []
+    with pd.read_csv(
+        input_file_path, skiprows=2, chunksize=5, header=None, sep=r"\s{1,}"
+    ) as reader:
         reader
         for chunk in reader:
-            print(chunk)
+            boards.append(chunk)
 
-    return draw_numbers
+    return draw_numbers, boards
